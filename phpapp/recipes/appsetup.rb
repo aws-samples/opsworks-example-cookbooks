0e78692c9845df6cb1e1d238a703d3a6f0945f72
Jeremy Lindblom <jeremeamia@gmail.com>
2014-01-13 15:18:13 -0800

    Updated Composer commands to follow latest docs

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -5,8 +5,8 @@ node[:deploy].each do |app_name, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install --no-dev
     EOH
   end
 
